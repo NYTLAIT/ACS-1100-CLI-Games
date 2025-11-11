b042735e2c8be9ac81ddb5ec59d8dfb6acb239bb
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2025-11-11 15:39:38 -0800

    Added a score system

--- flash-cards/flashcards.py
+++ flash-cards/flashcards.py
@@ -3,10 +3,15 @@ import json
 with open('me-capitals.json', 'r') as f:
     data = json.load(f)
 
+total = len(data["cards"])
+score = 0
+
 for i in data["cards"]:
     guess = input(i["q"] + ">")
     
-    if guess == i["a"]:
-        print("Correct!")
+    if guess.lower() == i["a"].lower():
+        score += 1
+        print(f"Correct! Current score: {score}/{total}")
     else:
-        print("Incorrect! The correct answer was", i["a"]) 
+        print("Incorrect! The correct answer was", i["a"])
+        print(f"Current score: {score}/{total}")

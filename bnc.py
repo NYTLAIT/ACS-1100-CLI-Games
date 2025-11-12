9e22b7e5f5fa5beeffe843d74ea595d5f3d4ba15
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2025-11-11 20:29:33 -0800

    Made Play Again loop using while and break

--- bnc.py
+++ bnc.py
@@ -2,26 +2,33 @@ from random import randint
 
 roles = ["Bear", "Ninja", "Cowboy"]
 
-player = input("Bear, Ninja, or Cowboy? > ")
 computer = roles[randint(0,2)]
 
-if computer == player:
-  print("DRAW!")
-elif computer == "Cowboy":
-  if player == "Bear":
-    print("You lose!", player, "is shot by", computer)
-  else:
-    print("You win!", player, "assasinates", computer)
-elif computer == "Bear":
-  if player == "Cowboy":
-    print("You win!", player, "shoots", computer)
-  else:
-    print("You lose!", player, "is mauled and eaten by", computer)
-elif computer == "Ninja":
-  if player == "Cowboy":
-    print("You lose!", player, "is assasinated by", computer)
-  else:
-    print("You win!", player, "mauls and eats", computer)
+player = False
+while player == False:
+    player = input("Bear, Ninja, or Cowboy? > ")
 
-else:
-    print(computer, player)
+    if computer == player:
+        print("DRAW!")
+    elif computer == "Cowboy":
+        if player == "Bear":
+            print("You lose!", player, "is shot by", computer)
+        else:
+            print("You win!", player, "assasinates", computer)
+    elif computer == "Bear":
+        if player == "Cowboy":
+            print("You win!", player, "shoots", computer)
+        else:
+            print("You lose!", player, "is mauled and eaten by", computer)
+    elif computer == "Ninja":
+        if player == "Cowboy":
+            print("You lose!", player, "is assasinated by", computer)
+        else:
+            print("You win!", player, "mauls and eats", computer)
+
+    play_again = input("Would you like to play again? (yes/no) > ")
+    if play_again == 'yes':
+        player = False
+        computer = roles[randint(0,2)]
+    else:
+        break

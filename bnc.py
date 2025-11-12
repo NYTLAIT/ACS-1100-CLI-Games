0735bfe13918eea7adfa0bad96c87a908357cf49
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2025-11-11 20:17:55 -0800

    Displays who won

--- bnc.py
+++ bnc.py
@@ -1,8 +1,27 @@
 from random import randint
 
-player = input("Bear, Ninja, or Cowboy? > ")
-print(player)
-
 roles = ["Bear", "Ninja", "Cowboy"]
+
+player = input("Bear, Ninja, or Cowboy? > ")
 computer = roles[randint(0,2)]
-print(computer)
+
+if computer == player:
+  print("DRAW!")
+elif computer == "Cowboy":
+  if player == "Bear":
+    print("You lose!", player, "is shot by", computer)
+  else:
+    print("You win!", player, "assasinates", computer)
+elif computer == "Bear":
+  if player == "Cowboy":
+    print("You win!", player, "shoots", computer)
+  else:
+    print("You lose!", player, "is mauled and eaten by", computer)
+elif computer == "Ninja":
+  if player == "Cowboy":
+    print("You lose!", player, "is assasinated by", computer)
+  else:
+    print("You win!", player, "mauls and eats", computer)
+
+else:
+    print(computer, player)
